8e8c02070d4971913db1a61fa85975fd163405c3
Vinh Giang <giangcamvinh@gmail.com>
2018-07-15 12:56:05 +0700

    init project

---
+++ src/components/Weather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="grid">
+    <div class="grid-item">{{ now | moment('ddd Do MMM') }}<br>{{ now | moment('h:mm a') }}</div>
+    <div class="grid-item"><img src="@/assets/images/sunny.png" alt=""></div>
+    <div class="grid-item">{{ temp }}</div>
+    <div class="grid-item">- Sweden <br>{{ citiName }}</div>
+    <div class="grid-item">
+      {{ condition }} <br>
+      {{ description }} <br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Weather',
+  data () {
+    return {
+      now: new Date(),
+      citiName: '',
+      temp: 0,
+      condition: '',
+      description: ''
+    }
+  },
+  async mounted () {
+    let city = 'Stockholm'
+
+    let response = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&appid=dfe15a41201d660911d013203832e676`)
+    let responseJson = await response.json()
+
+    let weatherInfo = responseJson.list[0]
+
+    if (weatherInfo) {
+      this.citiName = weatherInfo.name
+      this.temp = parseInt(weatherInfo.main.temp)
+      this.condition = weatherInfo.weather[0].main
+      this.description = weatherInfo.weather[0].description
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
